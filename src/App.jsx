8f720a870adf5e50c60bbcdd649519a619737d41
Basel Embaby <engbasel2025@gmail.com>
2025-09-05 17:53:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import FoodCard from './components/FoodCard.jsx'; 
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="root">
+    
+      <FoodCard 
+        name="بيتزا" 
+      />
+
+
+    </div>
+  );
+}
+
+export default App;
